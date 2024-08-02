ebd25ba781721d8b1f16c5a9c2af616a1ddc270b
Lucas Pennice <lucaspennice@gmail.com>
2024-08-02 10:53:12 -0300

    Initial Commit

---
+++ SkillTrees/SkillTreesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkillTreesApp.swift
+//  SkillTrees
+//
+//  Created by Lucas Pennice on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct SkillTreesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
